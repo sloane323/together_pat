9f7ac96b4f4c57e3f6b3005282ba1a91bed2a701
sloane323 <summerparty29@gmail.com>
2022-11-10 17:45:56 +0900

    new

--- src/Page/Header.jsx
+++ src/Page/Header.jsx
@@ -5,6 +5,7 @@ import { SlShare } from 'react-icons/sl';
 import {SlOptions} from 'react-icons/sl';
 import {SlClose} from 'react-icons/sl';
 
+
 import React, { useState, useEffect } from 'react';
 import { Menu, Button } from 'antd';
 import styled from 'styled-components';
@@ -12,92 +13,98 @@ import { BrowserView, MobileView } from 'react-device-detect';
 import { MenuOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
-const MenuList = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const NavTop = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  button {
-    background: black;
-    border: none;
-  }
-`;
+import { BiInfoCircle } from "react-icons/bi";
+import { BiHomeHeart } from "react-icons/bi";
+import { BiDoorOpen } from "react-icons/bi";
+import { BiBookmarkHeart } from "react-icons/bi";
+import { BiConversation } from "react-icons/bi";
 
 
 const Header = () => {
 
-  const [toggleMenu, setToggleMenu] = useState(false)
-  const [toggleBar, setToggleBar] = useState(true)
+  
 
-  const toggleChange = () => {
-    setToggleMenu(!toggleMenu)
-    setToggleBar(!toggleBar)
-  }
-
-  const onMenuClick = () => {
-    setToggleMenu(!toggleMenu)
-    setToggleBar(!toggleBar)
-  }
 
     return (
         <div id ="headerbig"  >
 
-           <div id = "titlename"> <span className ="e">너와나의 </span> 
-            <span className ="highlight">  특별한동행 </span>  </div>
+       <div id = "titlename"> <span className ="e">너와나의 </span> 
+       <span className ="highlight">  특별한동행 </span>  </div>
             
 
-          <div id = "icon-hd"> 
+       <div id = "icon-hd"> 
 
-             <div className="modalbig"> 
-             <div className="modalpa">
-            <button >
-             <SlShare size="25" color="#89CFD3" /></button>
-             </div>
-             <div className="modalpa2">
-            <button onClick={toggleChange} >
-             { toggleBar ? <SlOptions size="25" color="#89CFD3" /> : <SlClose size="25" color="#89CFD3" /> }
-             </button>
-             </div>
-             { toggleMenu &&
-          <Menu
-            defaultSelectedKeys={['1']}
-            mode="inline"
-            theme="light"
-            inlineCollapsed={toggleBar}
-            onClick={onMenuClick}
-          >
-            <Menu.Item key="subs">
-              구독하기
-            </Menu.Item>
-            <Menu.Item key="product">
-              상품 보기
-            </Menu.Item>
-            <Menu.Item key="cs">
-              고객센터
-            </Menu.Item>
-            <Menu.Item key="signin">
-              <Link to="/login">
-              로그인
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="signup">
-              <Link to="/signup">
-              회원가입
-              </Link>
-            </Menu.Item>
-          </Menu>
-        }
+      <div className="modalbig"> 
+      <div className="modalpa">
+      <button> <SlShare size="25" color="#89CFD3" /></button> </div>
+             
+      <div className="modalpa2" > 
+          <input type="checkbox" id="check_box" />
+    <label for="check_box">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+    <div id="side_menu">
+      <ul>
+        <li><div className="grid-it1">
+    <div className="down1">
+        <div id ="menu1" className='link-2'>    
+            <Link to='/' > <BiHomeHeart  size="35" color="#89CFD3"  />  </Link> </div> 
+        <div id ="menu2"> <p>HOME</p></div> 
+    </div>
+</div></li>
 
-          </div>
 
-          </div> 
-          
-          
-          
-          </div>
+        <li><div className="grid-it2">
+    <div className="down1">
+        <div id ="menu1" className='link-2'>
+        <Link to="/Info" >   <BiInfoCircle  size="35" color="#89CFD3"  /> </Link> </div>
+        <div id ="menu2">
+        <p>행사안내</p> </div>
+        </div> 
+    </div></li>
+
+
+        <li><div className="grid-it3">
+    <div className="down1">
+        <div id ="menu1" className='link-2'>
+        <Link to="/Course" > <BiDoorOpen  size="35" color="#89CFD3"  /></Link></div> 
+        <div id ="menu2">
+        <p>코스안내</p> </div>
+        </div> 
+    </div></li>
+
+        <li><div className="grid-it4">
+    <div className="down1">
+        <div id ="menu1" className='link-2'>
+        <Link to="/Booking" > <BiBookmarkHeart  size="35" color="#89CFD3"  />   </Link></div> 
+        <div id ="menu2">
+        <p>예약하기</p> </div>
+        </div> 
+</div></li>
+
+        <li><div className="grid-it5">
+    <div className="down1">
+        <div id ="menu1" className='link-2'>
+        <Link to="/Review" > <BiConversation  size="35" color="#89CFD3"  /> </Link></div> 
+        <div id ="menu2">
+        <p>후기작성</p> </div>
+        </div> 
+</div> </li>
+
+
+
+
+      </ul>
+    </div>
+
+                </div>
+       </div>
+                    
+      </div>
+      </div>
+
       );
 }
 
